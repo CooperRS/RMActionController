79a44a62721b1715cdf0da538d5c16ccee74249d
CooperRS <cooperrs@gmail.com>
2015-06-20 20:35:52 +0200

    Added Xcode UI Tests

---
+++ RMActionController-Demo-UITests/RMActionControllerPresentationTests.m
@@ -0,0 +1,86 @@
+//
+//  RMActionControllerPresentationTests.m
+//  RMActionController-Demo-UITests
+//
+//  Created by Roland Moers on 20.06.15.
+//  Copyright © 2015 Roland Moers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <XCTest/XCTest.h>
+
+@interface RMActionControllerPresentationTests : XCTestCase
+
+@end
+
+@implementation RMActionControllerPresentationTests
+
+#pragma mark - Lifecycle
+- (void)setUp {
+    [super setUp];
+    
+    self.continueAfterFailure = NO;
+    [[[XCUIApplication alloc] init] launch];
+}
+
+#pragma mark - Helper
+- (void)verifyCustomActionControllerIsVisibleInApp:(XCUIApplication *)app {
+    XCUIElement *actionController = [app descendantsMatchingType:XCUIElementTypeUnknown][@"ActionControllerView"];
+    XCUIElement *backgroundView = [app descendantsMatchingType:XCUIElementTypeUnknown][@"BackgroundView"];
+    XCUIElement *plusLabel = app.staticTexts[@"PlusLabel"];
+    XCUIElement *textLabel = app.staticTexts[@"TextLabel"];
+    XCUIElement *selectButton = app.buttons[@"Select"];
+    XCUIElement *cancelButton = app.buttons[@"Cancel"];
+    
+    XCTAssertNotNil(actionController);
+    XCTAssertNotNil(backgroundView);
+    XCTAssertNotNil(plusLabel);
+    XCTAssertNotNil(textLabel);
+    XCTAssertNotNil(selectButton);
+    XCTAssertNotNil(cancelButton);
+}
+
+#pragma mark - Tests
+- (void)testPresentingActionController {
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app.tables.staticTexts[@"Show sample action view controller…"] tap];
+    
+    [self verifyCustomActionControllerIsVisibleInApp:app];
+    
+    [app.buttons[@"Cancel"] tap];
+}
+
+- (void)testPresentingBlackActionController {
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app.tables.switches[@"BlackVersion"] tap];
+    [app.tables.staticTexts[@"Show sample action view controller…"] tap];
+    
+    [self verifyCustomActionControllerIsVisibleInApp:app];
+    
+    [app.buttons[@"Cancel"] tap];
+}
+
+- (void)testPresentingWithAllEffectsDisabled {
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app.tables.switches[@"BlurEffects"] tap];
+    [app.tables.switches[@"MotionEffects"] tap];
+    [app.tables.switches[@"BouncingEffects"] tap];
+    [app.tables.staticTexts[@"Show sample action view controller…"] tap];
+    
+    [self verifyCustomActionControllerIsVisibleInApp:app];
+    
+    [app.buttons[@"Cancel"] tap];
+}
+
+- (void)testPresentingFromPushViewController {
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app.tables.staticTexts[@"Push VC"] tap];
+    [app.tables.staticTexts[@"Show sample action view controller…"] tap];
+    
+    [self verifyCustomActionControllerIsVisibleInApp:app];
+    
+    [app.buttons[@"Cancel"] tap];
+    [app.navigationBars[@"Demo"].buttons[@"Demo"] tap];
+}
+
+@end
